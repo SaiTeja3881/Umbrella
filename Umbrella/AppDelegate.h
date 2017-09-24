3211f0855132982a7f67dba96a8d45c382b36aba
Saiteja Alle <alle.saiteja@gmail.com>
2017-09-24 10:41:42 -0400

    Initial Commit

---
+++ Umbrella/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Umbrella
+//
+//  Created by Saiteja Alle on 9/24/17.
+//  Copyright © 2017 Saiteja Alle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Umbrella/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Umbrella
+//
+//  Created by Saiteja Alle on 9/24/17.
+//  Copyright © 2017 Saiteja Alle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
